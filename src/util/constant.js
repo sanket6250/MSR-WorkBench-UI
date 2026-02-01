7ed7c2440120242f5606540f42341e9f96811f03
Sanket Mashalkar <sanketmashalkar12@gmail.com>
2026-02-01 22:50:11 +0530

    Changes in MSR-WorkBench UI

--- src/util/constant.js
+++ src/util/constant.js
@@ -2,5 +2,5 @@ export const APP_CONSTANTS =
 {
     //BACKEND_URL : 'http://127.0.0.1/auth/',
     AIML_SERVICE_END_POINT : 'http://127.0.0.1/api/',
-    BACKEND_URL : 'https://msr-workbench-server.onrender.com/api',
+    BACKEND_URL : 'https://msr-workbench-server.onrender.com/auth/api',
 }
