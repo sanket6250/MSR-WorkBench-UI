321a24831afac41a907c2aa47c27a5bcdccd9445
Sanket Mashalkar <sanketmashalkar12@gmail.com>
2026-02-12 00:33:01 +0530

    Added Document Extraction task

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,7 +58,7 @@ const Header = ()=>
 
         <nav className="navbar px-5 d-flex justify-content-between align-items-center" style={{minHeight: '30px',maxHeight: '72px', backgroundColor:'rgb(8 16 41)'}} >
             <div className="d-flex align-items-center gap-2 ">
-                <a href='/msr-workbench/' style={{display: 'flex' , gap: '5px' , textDecoration: 'none'}}>
+                <a href='/MSR-WorkBench-UI/' style={{display: 'flex' , gap: '5px' , textDecoration: 'none'}}>
                     <img src={assets.logo} alt="logo" width={32} height={32}/>
                     <span className="fw-bold fs-4 text-white">MSR Workbench</span>
                 </a>
