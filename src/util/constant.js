a0f1c3dc210da3229e7cb89a8056d661a1e3e6f1
Sanket Mashalkar <sanketmashalkar12@gmail.com>
2026-02-20 08:59:37 +0530

    Changes done for Bill Splitter

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,7 +1,7 @@
 export const APP_CONSTANTS = 
 {
-    BACKEND_URL : 'http://127.0.0.1/auth/',
+    //BACKEND_URL : 'http://127.0.0.1/auth/',
     //AIML_SERVICE_END_POINT : 'http://127.0.0.1/api/',
-    //BACKEND_URL : 'https://msr-workbench-server.onrender.com/auth/api',
+    BACKEND_URL : 'https://msr-workbench-server.onrender.com/auth/api',
     AIML_SERVICE_END_POINT : 'https://msr-workbench-ai-service-server.onrender.com/api/',
 }
